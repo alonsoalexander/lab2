0dcc610bf8b6089462a73ddb784e8a9464cae746
Alexander <alonsoal@chalmers.se>
2026-04-19 13:21:12 +0200

    Klar fram till 'Svårighetsgrad'

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lab2/widgets/difficulty_control.dart';
+import 'package:lab2/widgets/ingredient_control.dart';
+import 'package:lab2/widgets/kitchen_control.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
@@ -6,11 +9,7 @@ class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          _controlPanel(context), _recipeArea(context),
-        ],
-      ),
+      body: Row(children: [_controlPanel(context), _recipeArea(context)]),
     );
   }
 
@@ -18,12 +17,33 @@ class MainView extends StatelessWidget {
     return Container(
       width: width,
       color: const Color.fromARGB(255, 193, 210, 218),
+      child: const Column(
+        children: [
+          Text("Receptsök"),
+          Text("Hitta ett recept som passar genom att ändra"),
+          Text("Inställningarna nedanför"),
+          SizedBox(height: 20),
+
+          Row( children: [
+            Text("Ingrediens"),
+            IngredientControl(),
+        ]),
+          Row(children: [
+            Text("Kök"),
+            KitchenControl(),
+        ]),
+        SizedBox(height: 20),
+        Text('Svårighetsgrad'),
+        DifficultyControl(),
+
+        ],
+      ),
     );
   }
 
   Widget _recipeArea(BuildContext context) {
     return Expanded(
-      child: Container(color: const Color.fromARGB(255, 204, 216, 176),),
+      child: Container(color: const Color.fromARGB(255, 204, 216, 176)),
     );
   }
 }
